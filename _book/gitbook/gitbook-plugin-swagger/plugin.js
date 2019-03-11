b7eaa1d99b861801cf8ecfcda4f3d234b805f799
Miguel de Barros <miguel@debarros.me>
2019-03-11 17:36:19 +0000

    Revert "testing plugins"
    This reverts commit ba350d4fe55c5dbd6c1bb6c4b6c7d11bb8269a63.

--- _book/gitbook/gitbook-plugin-swagger/plugin.js
+++
@@ -1,7 +0,0 @@
-require(['gitbook', 'jquery'], function(gitbook, $) {
-  var items = $('.swagger > ul > li');
-  items.addClass('collapse');
-  items.find('h3').on('click', function(event) {
-    $(event.target).parent().toggleClass('collapse');
-  });
-});
