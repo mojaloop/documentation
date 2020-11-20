95fd52627be3f4e743a6554dd9d3294ce5e6233a
Lewis Daly <lewisdaly@me.com>
2020-11-20 16:00:49 +0800

    feat(ci/cd): fix bad reference to ./docs

--- scripts/_build_plantuml_all.sh
+++ scripts/_build_plantuml_all.sh
@@ -4,11 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH="${PATH}:${DIR}/../node_modules/.bin"
 
 ##
-# searches through `./docs` for plantuml sources
+# searches through repo for plantuml sources
 # and exports them using `node-plantuml`
 ##
 
-for i in $(find ./docs -name '*.p*uml'); do
+for i in $(find ${DIR}/.. -name '*.p*uml'); do
   echo "rendering .puml -> .svg for diagram diagram: $i"
   puml generate -s $i -o $(echo $i | sed 's/puml/svg/g' | sed 's/plantuml/svg/g' ) -i $(echo $i | sed -e 's;[^/]*$;;');
 done
