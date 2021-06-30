9e4f30907d043d40d314e8cdff4232678f88c13b
Lewis Daly <lewis@vesselstech.com>
2021-06-30 22:48:55 +0930

    feat(ci): add deployment to preview site in CI/CD (#311)
    * chore(readme): move readme to project root
    
    * chore(license): add the mojaloop license
    
    * chore(readme): minor improvements, add stub for deployment
    
    * feat(deploy): working on local deploy script
    
    * feat(deploy): basic local deploy working
    
    * chore(readme): add deploying section
    
    * chore(readme): add deploying section
    
    * chore(readme): add deploying section
    
    * chore(ci): working on branch build rules
    
    * chore(ci): working on branch build rules
    
    * chore(ci): working on deploy setup
    
    * chore(ci): working on deploy setup
    
    * chore(ci): working on deploy setup
    
    * chore(ci): chane build branch to `mojaloop-docs-2.0`

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 > This is the official documentation for the Mojaloop project.
 
+__Live Preview: [docs-preview.moja-lab.live](http://docs-preview.moja-lab.live/)__
+
 
 ## Building and testing locally
 
@@ -17,15 +19,28 @@ yarn
 yarn run dev
 ```
 
-
 ## Building the project
 Run `yarn run build` to build the project to render the static vuepress site for a deployment.
 
 ## Deploying the v2.0 Docs Preview
 
+We deploy these docs automatically with CircleCI upon merges to the `mojaloop-docs-2.0` branch.
+
+Go to: http://docs-preview.moja-lab.live/ to see the live site!
+
+The deploy script does not currently take versioning into account, but will in the future.
+
+> Note: for this to work, we have temporarily disabled the "only build pull requests" setting on Circleci
+> Someone please remember to turn that back on!
+
+### Deploying Manually
+
+You can also deploy them manually, by running:
 ```bash
-# TODO! - Lewis!
-```
+./scripts/_deploy_preview_s3.sh
+``` 
+
+Note that you need to have the `aws` cli, AWS access, and `aws-mfa` set up on your machine for this to work.
 
 ## License
 
---
+++ scripts/_deploy_preview_s3.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+
+# deploys the preview docs to an s3 bucket
+# I manually created the bucket: `mojaloop-docs-preview`
+# in the eu-west-2 region
+# and followed the guide: https://docs.aws.amazon.com/AmazonS3/latest/userguide/EnableWebsiteHosting.html
+# to manually configure the bucket for website hosting
+# And this guide: https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-serve-static-website/
+# to set up Cloudfront and Route53
+# "Using a REST API endpoint as the origin, with access restricted by an OAI"
+
+# The website should be available at:
+# http://mojaloop-docs-preview.s3-website.eu-west-2.amazonaws.com
+# or http://docs-preview.moja-lab.live/
+
+# Required tools:
+# - aws-cli
+# - aws-mfa (if running as user, not CI)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export AWS_REGION=eu-west-2
+export BUCKET_NAME=mojaloop-docs-preview
+
+set -e
+set -u
+
+# make sure we can actually list the s3 buckets
+aws s3 ls s3://${BUCKET_NAME}
+
+# build
+rm -rf ${DIR}/../build
+cd ${DIR}/../vuepress
+yarn
+yarn build
+
+mv ${DIR}/../vuepress/docs/.vuepress/dist ${DIR}/../build
+
+# TODO: can we be smart about docs versions here? maybe every minor version we can keep...
+
+# upload built files to s3 
+aws s3 sync ${DIR}/../build s3://${BUCKET_NAME} \
+  --acl public-read
+
+echo "go to: "
+echo "http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com"
+echo "or http://docs-preview.moja-lab.live/ to see the live site!"
